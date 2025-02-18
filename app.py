50c44f7417ee5b5fe312413b2aedb051ed4cfa67
Lazzy-bug <imprithvi2003@gmail.com>
2025-02-18 20:11:27 +0530

    updated db

--- app.py
+++ app.py
@@ -61,5 +61,9 @@ def deletepost():
     db.session.commit()
     return redirect(url_for('index'))
 
+@app.before_request
+def create_tables():
+    db.create_all()
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
